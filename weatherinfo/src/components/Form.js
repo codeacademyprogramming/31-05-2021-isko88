840036343b00f8c0d77930eeffeae3e55ea4b264
isko88 <ismayilj@code.edu.az>
2021-06-01 08:34:15 +0400

    Form component added

--- weatherinfo/src/components/Form.js
+++ weatherinfo/src/components/Form.js
@@ -14,7 +14,7 @@ export default function Form() {
         if (evt.key === "Enter") {
             getCityWeather(evt.target.value, dispatch)
         }
-    }, [getCityWeather,dispatch])
+    }, [getCityWeather, dispatch])
 
     return (
         <>
@@ -22,14 +22,14 @@ export default function Form() {
                 <InputLabel htmlFor="city" >City</InputLabel>
                 <Input id="city" onKeyUp={(evt) => handleEnter(evt)} aria-describedby="my-helper-text" />
             </FormControl>
-            
+
             <List component="nav" aria-label="contacts">
-               {datalist.data ? datalist.data.map(data=>{
-                   {console.log(data)}
-                <ListItem button>
+                {datalist.data ? datalist.data.map(data => {
+                    { console.log(data) }
+                    <ListItem button>
                         <ListItemText primary="Chelsea Otakan" />
                     </ListItem>
-               }) : "Loading"}
+                }) : "Loading"}
             </List>
         </>
     )
