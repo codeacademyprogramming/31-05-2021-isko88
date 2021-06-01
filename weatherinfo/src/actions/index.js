4a5ea0fcc49fbfd911d0e23ab8f5667fead17b1e
isko88 <ismayilj@code.edu.az>
2021-06-01 08:33:56 +0400

    action added

--- weatherinfo/src/actions/index.js
+++ weatherinfo/src/actions/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import axios from 'axios'
-import {WEATHER} from './consts'
-export default function getCityWeather(city,dispatch) {
-   
+import { WEATHER } from './consts'
+export default function getCityWeather(city, dispatch) {
+
     axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=16596fe956171a7376f2ba91213e3499`)
-  .then(({data})=>{
-      console.log("action",data)
-      dispatch({
-          type:WEATHER.GET_WEATHER_BY_CITY,
-          payload:data
-      })
-  })
-  .catch(err=>{console.log(err)})
+        .then(({ data }) => {
+            console.log("action", data)
+            dispatch({
+                type: WEATHER.GET_WEATHER_BY_CITY,
+                payload: data
+            })
+        })
+        .catch(err => { console.log(err) })
 }
