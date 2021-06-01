25dc661951018bc58f36089bf8207552ced98548
isko88 <ismayilj@code.edu.az>
2021-06-01 08:34:50 +0400

    reducers added

--- weatherinfo/src/reducers/index.js
+++ weatherinfo/src/reducers/index.js
@@ -1,17 +1,17 @@
 import { WEATHER } from "../actions/consts";
 
 const INITIAL_STATE = {
-    status:false,
-    data:[]
+    status: false,
+    data: []
 }
-export function weatherReducer(state = INITIAL_STATE,action){
-    console.log("reducer",action)
+export function weatherReducer(state = INITIAL_STATE, action) {
+    console.log("reducer", action)
     switch (action.type) {
         case WEATHER.GET_WEATHER_BY_CITY:
-            return{
+            return {
                 ...state,
-                data:action.payload,
-                status:true
+                data: action.payload,
+                status: true
             }
         default:
             return state;
