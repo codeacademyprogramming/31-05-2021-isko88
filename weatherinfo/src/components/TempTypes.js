53d2ad60b1df3cc5a5e6b2339067e735c87145ba
isko88 <ismayilj@code.edu.az>
2021-06-01 08:34:35 +0400

    temp types added

--- weatherinfo/src/components/TempTypes.js
+++ weatherinfo/src/components/TempTypes.js
@@ -4,9 +4,9 @@ import React from 'react'
 export default function TempTypes() {
     return (
         <RadioGroup aria-label="Temps" name="Temps" >
-        <FormControlLabel value="Kelvin" control={<Radio />} label="Kelvin" />
-        <FormControlLabel value="Celcius" control={<Radio />} label="Celcius" />
-        <FormControlLabel value="Fehrenheight" control={<Radio />} label="Fehrenheight" />
-      </RadioGroup>
+            <FormControlLabel value="Kelvin" control={<Radio />} label="Kelvin" />
+            <FormControlLabel value="Celcius" control={<Radio />} label="Celcius" />
+            <FormControlLabel value="Fehrenheight" control={<Radio />} label="Fehrenheight" />
+        </RadioGroup>
     )
 }
